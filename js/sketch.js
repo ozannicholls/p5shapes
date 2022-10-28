c6961f1703f0e54d68cf63f0d57041da91ddac3f
ozannicholls <116882906+ozannicholls@users.noreply.github.com>
2022-10-28 12:26:22 +0100

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,15 @@
+//Setup function runs once at the start
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 600);
     canvas.parent("p5container");
 }
 
+//Draw function runs at 60fps in a continuous loop
 function draw() {
-    background(220);
+    background(220);//greyscale for background
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(300, 300, 400);
+    fill(0, 0, 0)
+    square(270, 270, 60);
 }
